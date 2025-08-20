66a7b6232acad020d71da818ac469ef11c057cda
Harish Kumar G <harishkum9912@gmail.com>
2025-08-20 18:00:31 +0530

    Create bubblesort_Anagram.java

---
+++ Java_Harish_2024/src/Day3/bubblesort_Anagram.java
@@ -0,0 +1,24 @@
+package Day3;
+
+public class Sorter {
+    // Bubble sort implementation for char arrays
+    public static void bubbleSort(char[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    char temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;  // Array is already sorted
+            }
+        }
+    }
+}

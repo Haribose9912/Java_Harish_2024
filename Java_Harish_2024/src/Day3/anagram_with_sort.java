e703e9303ec5b6233ef29704c9465498b8817a51
Harish Kumar G <harishkum9912@gmail.com>
2025-08-20 18:02:47 +0530

    Update anagram_with_sort.java

--- Java_Harish_2024/src/Day3/anagram_with_sort.java
+++ Java_Harish_2024/src/Day3/anagram_with_sort.java
@@ -21,9 +21,11 @@ public class Anagram {
         char[] arr1 = str1.toCharArray();
         char[] arr2 = str2.toCharArray();
 
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-
+        // Arrays.sort(arr1);  //built in sort
+        // Arrays.sort(arr2); 
+        bubblesort_Anagram.bubbleSort(arr1);
+        bubblesort_Anagram.bubbleSort(arr2);
+        
         // Compare sorted arrays
         if (Arrays.equals(arr1, arr2)) {
             System.out.println(str1 + " and " + str2 + " are anagrams.");

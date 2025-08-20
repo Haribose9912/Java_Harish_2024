4abce5c610855e45e200b2b824d404647b793403
Harish Kumar G <harishkum9912@gmail.com>
2025-08-20 17:54:22 +0530

    Create anagram_with_sort.java

---
+++ Java_Harish_2024/src/Day3/anagram_with_sort.java
@@ -0,0 +1,34 @@
+package Day3;
+
+import java.util.Arrays;
+
+public class Anagram {
+    public static void main(String[] args) {
+        String str1 = "rood";
+        String str2 = "door";
+
+        // Normalize input
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
+        // Quick length check
+        if (str1.length() != str2.length()) {
+            System.out.println(str1 + " and " + str2 + " are not anagrams.");
+            return;
+        }
+
+        // Convert to char arrays and sort
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        // Compare sorted arrays
+        if (Arrays.equals(arr1, arr2)) {
+            System.out.println(str1 + " and " + str2 + " are anagrams.");
+        } else {
+            System.out.println(str1 + " and " + str2 + " are not anagrams.");
+        }
+    }
+}

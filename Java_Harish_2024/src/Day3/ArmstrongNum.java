f9ca8cb4cbf0dacaee9e44217d2d723c50354b84
Harish Kumar G <harishkum9912@gmail.com>
2025-08-20 16:34:39 +0530

    Update ArmstrongNum.java

--- Java_Harish_2024/src/Day3/ArmstrongNum.java
+++ Java_Harish_2024/src/Day3/ArmstrongNum.java
@@ -2,6 +2,29 @@ package Day3;
 import java.util.*;
 
 public class ArmstrongNum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	  
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int originalNum = num;
+        
+        // Count digits
+        int digits = String.valueOf(num).length();
+        
+        int sum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, digits);
+            num /= 10;
+        }
+        
+        if (sum == originalNum) {
+            System.out.println(originalNum + " is an Armstrong number.");
+        } else {
+            System.out.println(originalNum + " is NOT an Armstrong number.");
+        }
+        
+        sc.close();
+    }
 }

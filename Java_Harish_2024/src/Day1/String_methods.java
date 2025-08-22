dba33830ca7e3d0860eba85a87d7786e1b9f9f0c
Harish Kumar G <93115324+Haribose9912@users.noreply.github.com>
2025-08-22 12:21:34 +0530

    added

--- Java_Harish_2024/src/Day1/String_methods.java
+++ Java_Harish_2024/src/Day1/String_methods.java
@@ -1,5 +1,90 @@
 package Day1;
 
-public class String_methods {
+import java.util.*;
 
+public class String_methods {
+    public static void main(String[] args) {
+        // =============================
+        // 1. STRING METHODS
+        // =============================
+        String str = "  Java Programming  ";
+        String str2 = "java programming";
+
+        System.out.println("=== String Methods ===");
+        System.out.println("Length: " + str.length());
+        System.out.println("CharAt(5): " + str.charAt(5));
+        System.out.println("Equals: " + str.trim().equals(str2));
+        System.out.println("EqualsIgnoreCase: " + str.trim().equalsIgnoreCase(str2));
+        System.out.println("CompareTo: " + "apple".compareTo("banana"));
+        System.out.println("Contains 'Pro': " + str.contains("Pro"));
+        System.out.println("IndexOf 'a': " + str.indexOf("a"));
+        System.out.println("LastIndexOf 'a': " + str.lastIndexOf("a"));
+        System.out.println("Substring(2,6): " + str.substring(2, 6));
+        System.out.println("Concat: " + str.concat(" is fun"));
+        System.out.println("Replace 'a'->'o': " + str.replace('a', 'o'));
+        System.out.println("ToUpperCase: " + str.toUpperCase());
+        System.out.println("Trim: '" + str.trim() + "'");
+
+        String csv = "red,green,blue";
+        String[] colors = csv.split(",");
+        System.out.println("Split: ");
+        for (String color : colors) System.out.println(color);
+
+        String joined = String.join("-", "2025", "08", "19");
+        System.out.println("Joined: " + joined);
+
+        // =============================
+        // 2. STRINGBUILDER METHODS
+        // =============================
+        StringBuilder sb = new StringBuilder("Hello");
+
+        System.out.println("\n=== StringBuilder Methods ===");
+        sb.append(" World");                      // append
+        System.out.println("Append: " + sb);
+
+        sb.insert(5, " Java");                   // insert
+        System.out.println("Insert: " + sb);
+
+        sb.delete(5, 10);                        // delete
+        System.out.println("Delete: " + sb);
+
+        sb.replace(6, 11, "Universe");           // replace
+        System.out.println("Replace: " + sb);
+
+        sb.reverse();                            // reverse
+        System.out.println("Reverse: " + sb);
+
+        sb.reverse(); // make it normal again
+        System.out.println("CharAt(1): " + sb.charAt(1));
+        System.out.println("Length: " + sb.length());
+        System.out.println("Capacity: " + sb.capacity()); // default 16 + length
+        System.out.println("Substring(0,5): " + sb.substring(0,5));
+
+        // =============================
+        // 3. STRINGBUFFER METHODS
+        // =============================
+        StringBuffer sbuf = new StringBuffer("Welcome");
+
+        System.out.println("\n=== StringBuffer Methods ===");
+        sbuf.append(" User");                    // append
+        System.out.println("Append: " + sbuf);
+
+        sbuf.insert(7, " Java");                 // insert
+        System.out.println("Insert: " + sbuf);
+
+        sbuf.delete(7, 12);                      // delete
+        System.out.println("Delete: " + sbuf);
+
+        sbuf.replace(0, 7, "Hello");             // replace
+        System.out.println("Replace: " + sbuf);
+
+        sbuf.reverse();                          // reverse
+        System.out.println("Reverse: " + sbuf);
+
+        sbuf.reverse(); // normal again
+        System.out.println("CharAt(2): " + sbuf.charAt(2));
+        System.out.println("Length: " + sbuf.length());
+        System.out.println("Capacity: " + sbuf.capacity());
+        System.out.println("Substring(0,5): " + sbuf.substring(0,5));
+    }
 }
--- Java_Harish_2024/src/Day1/practise.java
+++ Java_Harish_2024/src/Day1/practise.java
@@ -1,11 +1,29 @@
 package Day1;
-import java.util.*;
-public class practise {
 
-	public static void main(String[] args) {
-	
-	
-	
-	}
-	
+import java.util.*;
+
+
+public class practise{
+  
+  public static void main (String[] args) {
+    
+  String n ="hariesharanya";
+  ArrayList<Character> nums = new ArrayList<>();
+ 
+  for( int i=0;i<n.length();i++){
+    char ch = n.charAt(i);
+    nums.add(ch);
+  }
+  
+  ArrayList<Object> dl = new ArrayList<>();
+  ArrayList<Object> ul = new ArrayList<>();
+  for(Object num:nums){
+    if(!ul.contains(num)){
+      ul.add(num);
+    }else{
+      dl.add(num);
+    }
+  }System.out.println("unique list: "+ul);
+System.out.println("dupli list: "+dl);
+}
 }

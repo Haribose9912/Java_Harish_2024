9d925e0ed04d63db5fda004a3f0fe2a8aae517bd
Harish Kumar G <harishkum9912@gmail.com>
2025-08-29 22:08:36 +0530

    Update Reverse_even_words.java

--- Java_Harish_2024/src/StringBuilder/Reverse_even_words.java
+++ Java_Harish_2024/src/StringBuilder/Reverse_even_words.java
@@ -4,26 +4,25 @@ public class Reverse_even_words {
 
 
 	    public static void main(String[] args) {
-	    	String s = "welcome to mindtree hyderabad";
-	        
-	        String[] ss = s.split(" ");
-	     
-	        StringBuilder sb = new StringBuilder();
-	     
-	        for (int i = 0; i < ss.length; i++) {
-	            if ((i + 1) % 2 == 0) { // Check if index is even (1-based)
-	                String revs = "";
-	                for (int j = 0; j < ss[i].length(); j++) {
-	                    char c = ss[i].charAt(j);
-	                    revs = c + revs; // Manually reverse the word
-	                }
-	                sb.append(revs); // Append the reversed word
-	            } else {
-	                sb.append(ss[i]); // Keep the word unchanged
-	            }
-	            sb.append(" "); // Add space after each word
-	        }
-	     
-	        System.out.println("Result: " + sb.toString().trim());
+ String s = "hariesh am here how are you bye";
+
+        String words[] = s.split(" ");
+        System.out.println(Arrays.toString(words));
+
+        ArrayList<String> sl = new ArrayList<>();
+        StringBuilder rev = new StringBuilder();
+
+        for (int i = 2; i < words.length; i++) {
+            if (i % 2 == 0) {
+                sl.add(words[i] + ":" + i);
+                // Use words[i] instead of word
+                for (int j = words[i].length() - 1; j >= 0; j--) {
+                    rev.append(words[i].charAt(j));
+                }
+                rev.append(" ");
+            }
+        }
+        System.out.println(sl);
+        System.out.println(rev);
     }
 }

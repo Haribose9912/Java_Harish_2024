5385faf2341aac6a35aa30826d3ce2048877880e
Harish Kumar G <harishkum9912@gmail.com>
2025-08-29 21:08:05 +0530

    Create SecondLargestArray.java

---
+++ Java_Harish_2024/src/Day2/SecondLargestArray.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class SecondLargestArray {
+    public static void main(String[] args) {
+        int[] arr = {12, 35, 1, 10, 34, 1};
+        Arrays.sort(arr);
+        System.out.println("Second largest: " + arr[arr.length - 2]);
+    }
+}
+
+
+// public class SecondLargest {
+//     public static void main(String[] args) {
+//         int[] arr = {12, 35, 1, 10, 34, 1};
+
+//         int first = Integer.MIN_VALUE, second = Integer.MIN_VALUE;
+
+//         for (int num : arr) {
+//             if (num > first) {
+//                 second = first;
+//                 first = num;
+//             } else if (num > second && num != first) {
+//                 second = num;
+//             }
+//         }
+
+//         if (second == Integer.MIN_VALUE) {
+//             System.out.println("No second largest element");
+//         } else {
+//             System.out.println("Second largest element is: " + second);
+//         }
+//     }
+// }
+

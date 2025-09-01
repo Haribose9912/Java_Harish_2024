e3c4fcdbc138f9ba93b5c74218c65ad883e01f09
Harish Kumar G <harishkum9912@gmail.com>
2025-09-01 19:16:47 +0530

    Update PrimeNum.java

--- Java_Harish_2024/src/Day3/PrimeNum.java
+++ Java_Harish_2024/src/Day3/PrimeNum.java
@@ -1,22 +1,27 @@
 package Day3;
 
 public class PrimeNum {
-	 public static boolean isPrime(int n) {
-	        if (n < 2) return false;
-	        for (int i = 2; i * i <= n; i++) {
-	            if (n % i == 0) return false;
-	        }
-	        return true;
-	    }
 
     public static void main(String[] args) {
         int n = 17;
-        if (isPrime(n)) {
+        int count = 0;
+
+        if (n < 2) {
+            System.out.println(n + " is not a prime number.");
+            return;
+        }
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                count++; // if any factor found, increment count
+                break;   // no need to check further
+            }
+        }
+
+        if (count == 0) {
             System.out.println(n + " is a prime number.");
         } else {
             System.out.println(n + " is not a prime number.");
         }
-
-    }    // Method to check if a number is prime
-   
+    }
 }

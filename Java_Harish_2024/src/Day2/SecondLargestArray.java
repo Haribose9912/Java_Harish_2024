f5f3f57ad36f7b188bcbfc8487c97e707d3e5735
Harish Kumar G <harishkum9912@gmail.com>
2025-09-01 21:52:19 +0530

    Update SecondLargestArray.java

--- Java_Harish_2024/src/Day2/SecondLargestArray.java
+++ Java_Harish_2024/src/Day2/SecondLargestArray.java
@@ -8,7 +8,7 @@ public class SecondLargestArray {
     }
 }
 
-
+//WAY -2 
 // public class SecondLargest {
 //     public static void main(String[] args) {
 //         int[] arr = {12, 35, 1, 10, 34, 1};
@@ -44,5 +44,21 @@ public class SecondLargestArray {
 //         }
 //     }
 // }
+
+
+//Way- 3
+// int[] arr = {12, 35, 1, 10, 34, 1};
+// int first = Integer.MIN_VALUE, second = Integer.MIN_VALUE;
+
+// for (int num : arr) {
+//     if (num > first) {
+//         second = first;
+//         first = num;
+//     } else if (num > second && num != first) {
+//         second = num;
+//     }
+// }
+// System.out.println(second == Integer.MIN_VALUE ? "No second largest" : "Second largest: " + second);
+
 
 

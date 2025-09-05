91da5fe095a34cdfb097a01a6e3c9edc1afc0caf
Harish Kumar G <harishkum9912@gmail.com>
2025-09-05 22:57:29 +0530

    Update DataProvider_usage.java

--- Java_Harish_2024/src/TestNG/DataProvider_usage.java
+++ Java_Harish_2024/src/TestNG/DataProvider_usage.java
@@ -2,4 +2,20 @@ package TestNG;
 
 public class DataProvider_usage {
 
+@DataProvider(name="data")
+public Object[][] userData(){
+return new Object[][]{
+  {"harish","Hari@9911"},
+  {"savvi","Hari@3344"}
+};
+}
+
+@Test(dataProvider="data")
+public void testUser(String Username,String Password){
+    WebElement uname = driver.findElement(By.id("userid"));
+    WebElement pass = driver.findElement(By.id("passwd"));
+    uname.sendKeys(Username);
+    pass.sendKeys(Password);
+  }
+  
 }

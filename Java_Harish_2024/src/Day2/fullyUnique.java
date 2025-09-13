05471549b9916ded175d41f82bde788d0bda42d3
Harish Kumar G <harishkum9912@gmail.com>
2025-09-13 15:05:08 +0530

    Update fullyUnique.java

--- Java_Harish_2024/src/Day2/fullyUnique.java
+++ Java_Harish_2024/src/Day2/fullyUnique.java
@@ -6,16 +6,23 @@ int nums []={5,6,2,6,7,2,9,9};
 System.out.println(Arrays.toString(nums));
 ArrayList<Integer> uni = new ArrayList<>();
 ArrayList<Integer> fulluni = new ArrayList<>();
+ArrayList<Integer> dups = new ArrayList<>();
     int count =0;
 for(int num:nums){
   if(!uni.contains(num)){
      count++;
      uni.add(num);
-     fulluni.add(num); // Add to fulluni initially
+     if(count < 2){
+         fulluni.add(num);
+     }
   } else {
-     fulluni.remove(Integer.valueOf(num)); // Remove if duplicate
+     dups.add(num);
   }
-   
+}
+for(int num:uni){
+  if(!dups.contains(num) && !fulluni.contains(num)){
+     fulluni.add(num);
+  }
 }
  System.out.println(uni);
  System.out.println(fulluni);

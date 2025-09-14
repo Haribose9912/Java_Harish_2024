b5b0fa85b7fabaa73b7605aaeb7772d73b9441e1
Harish Kumar G <harishkum9912@gmail.com>
2025-09-14 20:50:28 +0530

    Create custexp.java

---
+++ customException/custexp.java
@@ -0,0 +1,11 @@
+public class custexp extends Exception{
+  
+  public custexp(String message){
+    super(message);
+  }
+  
+   public custexp(String message, Throwable cause) {
+        super(message, cause);
+    }
+  
+}

aadb3437ccc51d408f09ad4d840d826c935bcf99
Harish Kumar G <harishkum9912@gmail.com>
2025-09-14 20:49:43 +0530

    Create testprime.java

---
+++ customException/testprime.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class testprime {
+  
+public static int vote(int age)throws custexp{
+  if(age<18){
+    throw new custexp("your age is not valid for vote");
+  }
+  else if(age>18){
+    System.out.println("Successfully your vote has been registered");
+  }
+  return age;
+}
+
+
+
+public static void main (String[] args) {
+  try{
+   vote(16);
+  }catch(custexp e){
+      System.out.println("caught exception: "+e.getMessage());    
+      e.printStackTrace();
+  }
+  
+
+  }
+}
